51660b785f4c0856d737d5e695e25c94431a9083
kyoeeeen513-alt <kyoeeeen513@gmail.com>
2026-06-28 10:27:02 +0900

    Update charge.js

--- api/charge.js
+++ api/charge.js
@@ -8,7 +8,7 @@ module.exports = async (req, res) => {
   if (req.method !== 'POST') { res.status(405).json({ error: 'Method not allowed' }); return; }
 
   try {
-    const stripe = Stripe('sk_test_51TmkVPCG9UHvLerJVQiQOS5yGiTkL1YKKRbDleR0x8EycFF96q6DOgPxyeQjpdhZjJvkUUBvWGe1pYYiXiUH0YGO004GdiXE4L');
+    new const stripe = Stripe('sk_test_51TmkVPCG9UHvLerJVQiQOS5yGiTkL1YKKRbDleR0x8EycFF96q6DOgPxyeQjpdhZjJvkUUBvWGe1pYYiXiUH0YGO004GdiXE4L');
     const { customer_id, amount, description } = req.body;
 
     // 顧客のデフォルトカードを取得
